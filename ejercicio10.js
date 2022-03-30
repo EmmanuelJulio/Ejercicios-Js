f62dd103ef7f8e5f032ad613bf0a0232fe5b5025
emmanuel julio <emajulio.ej@gmail.com>
2022-03-29 21:13:28 -0300

    Agregados mas cambios

---
+++ ejercicio10.js
@@ -0,0 +1,13 @@
+/*
+Enunciado dado un numero mostrar una escaleras 
+con escalones de [-] usando el numero para los niveles de la escalera
+*/
+
+function escalera(numero){
+    const escalon = "[-]"
+    for (let index = 0; index < numero; index++) {
+        console.log(escalon.repeat(index+1)) 
+    }
+
+}
+escalera(15)
--- ejercicio9.js
+++ ejercicio9.js
@@ -1,3 +1,29 @@
 /* dados dos array devbolver solo los elementos comunes en los dos arrays */
 
 /*ElementosComunes() */
+
+/// mi solucion
+const array1 = [1,2,3,4,5];
+const array2 = [2,3,6,7,12];
+
+function ElementosComunes(array1,array2){
+    let arrayResultado =[]
+    array1.forEach(element1 => {
+        array2.forEach(element2=>{
+            if(element1 === element2){
+                arrayResultado.push(element1)
+            }
+        })
+    });
+    console.log(arrayResultado);
+}
+
+ElementosComunes(array1,array2);
+
+function ElementosComunesProf(array1){
+    const filtrado = array1.filter(elemento =>{
+        array2.includes(elemento)
+    })
+    console.log(filtrado)
+}
+ElementosComunes(array1,array2)
